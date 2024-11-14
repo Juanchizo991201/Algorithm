5c5f123589a7668934f570482aa54f8f35e43985
jjmontenegro <jj.montenegro@uniandes.edu.co>
2024-11-13 23:19:42 -0500

    project structure update

--- src/Main.java
+++ src/Main.java
@@ -14,14 +14,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<Integer> numbers = new ArrayList<>();
-
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(5);
-        numbers.add(3);
-
-        System.out.println(numbers);
+        System.out.println(Solution.polygonArea("Rectangle", 7 ,4));
     }
-
 }
